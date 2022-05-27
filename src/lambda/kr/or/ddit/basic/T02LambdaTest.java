fc7e12f50cdb12f7d906f8a610ad4670f85e17f4
JayAl <sjf95@naver.com>
2022-05-27 17:32:50 +0900

    Lambda Exam.

--- src/lambda/kr/or/ddit/basic/T02LambdaTest.java
+++ src/lambda/kr/or/ddit/basic/T02LambdaTest.java
@@ -1,5 +1,8 @@
 package lambda.kr.or.ddit.basic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class T02LambdaTest {
 	public static void main(String[] args) {
 		// 람다식을 사용하지 않았을 경우
@@ -15,7 +18,7 @@ public class T02LambdaTest {
 		LambdaTestInterface1 lam2 = () -> System.out.println("반갑습니다. \n 람다식으로 처리하는 방식입니다.");
 		lam2.test();
 		System.out.println("-------------------------------------------");
-		
+
 //		람다식의 작성 방법
 //		
 //		기본형식) (자료형이름 매개변수명, ....) -> {실행문들;}
@@ -24,7 +27,7 @@ public class T02LambdaTest {
 //		예) (int a) -> {System.out.println(a)};
 //		    (a) -> {System.out.println(a)};
 //		
-//		2) 매개변수가 1개일 경우에는 괄호'{}'를 생략할 수 있다.
+//		2) 매개변수가 1개일 경우에는 괄호'()'를 생략할 수 있다.
 //			(단, '자료형이름'을 지정할 경우에는 괄호를 생략할 수 없다.)
 //		예) a->{System.out.println();}
 //		
@@ -41,5 +44,59 @@ public class T02LambdaTest {
 //		
 //		6) 실행문에 return문만 있을 경우 return명령과 '{}'를 생략할 수 있다.
 //		예) (a, b) -> a + b
+
+		LambdaTestInterface2 lam3 = (int z) -> {
+			int result = z + 100;
+			System.out.println("result = " + result);
+		};
+		lam3.test(30);
+
+		LambdaTestInterface2 lam4 = z -> {
+			int result = z + 300;
+			System.out.println("result = " + result);
+		};
+		lam4.test(60);
+
+		LambdaTestInterface2 lam5 = z -> System.out.println("result = " + (z + 500));
+		lam5.test(90);
+
+		System.out.println("------------------------------------");
+		// ==========================================================
+
+		LambdaTestInterface3 lam6 = (int x, int y) -> {
+			int r = x + y;
+			return r;
+		};
+		int k = lam6.test(20, 50);
+		System.out.println("k = " + k);
+
+		LambdaTestInterface3 lam7 = (x, y) -> {
+			return x + y;
+		};
+		k = lam7.test(80, 50);
+
+		LambdaTestInterface3 lam8 = (x, y) -> x + y;
+		k = lam8.test(100, 200);
+		System.out.println("k = " + k);
+
+		LambdaTestInterface3 lam9 = (x, y) -> x > y ? x : y;
+		k = lam9.test(100, 200);
+		System.out.println("k = " + k);
+
+		// =======================================================
+
+		List<String> list = new ArrayList<>();
+		list.add("홍길동");
+		list.add("이몽룡");
+		list.add("이순신");
+		list.add("일지매");
+		list.add("변학도");
+
+		for (String str : list) {
+			System.out.println(str);
+		}
+		
+		list.forEach(str->{System.out.println(str);});
+
 	}
 }
